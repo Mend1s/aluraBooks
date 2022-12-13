3504bc9b564f2818f4ad81fe25e80a10e4317bf9
mend1s <andremendesskt99@gmail.com>
2022-12-13 17:01:26 -0300

    metodo sort

---
+++ app/metodoSort.js
@@ -0,0 +1,7 @@
+const btnOrdenacaoPorPreco = document.getElementById('btnOrdenarPorPreco')
+btnOrdenacaoPorPreco.addEventListener('click', ordenarLivrosPorPreco)
+
+function ordenarLivrosPorPreco() {
+    let livrosOrdenados = livros.sort((a, b) => a.preco - b.preco)
+    exibirOsLivrosNaTela(livrosOrdenados)
+}
